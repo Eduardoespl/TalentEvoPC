69f0de2f623e9924919814c00d7955ec6c825fe7
Eduardoespl <eduardoesp3317@gmail.com>
2024-09-14 10:24:29 -0600

    Login styles update

--- src/Login.tsx
+++ src/Login.tsx
@@ -51,7 +51,6 @@ function Login() {
             name='password'
             placeholder='Contraseña'
           />
-          <a onClick={handleLink} href='#'>No tengo una cuenta</a>
           <button type='submit'>Iniciar sesión</button>
           {error && <p>{error}</p>}
         </form>
